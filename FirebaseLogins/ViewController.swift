7dba21df1610e989bb9fec088a08884998a241f1
Joe Kim <josephkim0224@gmail.com>
2017-03-23 13:50:30 -0400

    Facebook button added

--- FirebaseLogins/ViewController.swift
+++ FirebaseLogins/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let loginButton = FBSDKLoginButton()
+        
+        view.addSubview(loginButton)
+        loginButton.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, topConstant: 50, leftConstant: 16, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width - 32, heightConstant: 30)   // As of 4.19.0, the height is now capped at 30 for some reason...
+        
+        loginButton.delegate = self
     }
-
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("MOR: Successfully logged out of Facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print("\(error)")
+        }
+        
+        print("MOR: Successfully logged in with Facebook")
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirebaseLogins' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebaseLogins
+  pod 'FBSDKCoreKit'
+  pod 'FBSDKShareKit'
+  pod 'FBSDKLoginKit'
+
+end
